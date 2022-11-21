ac0b86a93bcdef1d02629ad10115be002d259718
kevindavidsonebio <kvndvdsnebio@gmail.com>
2022-11-21 17:23:00 +0800

    wip

--- database/factories/StudentFactory.php
+++ database/factories/StudentFactory.php
@@ -17,7 +17,10 @@ class StudentFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'firstname' => $this->faker->firstName,
+            'lastname' => $this->faker->lastName,
+            'email' => $this->faker->email,
+            'phone' => $this->faker->phone,
         ];
     }
 }
